54faf42e1030390ebf170264e011e14fade67695
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-10-04 23:05:37 -0400

    Simple player controller

---
+++ Assets/Scripts/PlanetController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour
+{
+
+    public float moveSpeed = 3;
+    public float gravityStrength = 5;
+    public float gravityRange = 3;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb2d.AddForce(new Vector2(horizontal, vertical));
+    }
+}
