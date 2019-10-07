11ca2c0fb99e6748b03a3904929e2ec14182d7a8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-10-07 15:13:54 -0400

    Added menu gui

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+
+    private static MenuManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(instance.gameObject);
+        }
+        instance = this;
+
+        //Check to make sure game scene is loaded
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
